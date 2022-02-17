a26d6f437e0d2dab28d40db71fdfd39300f5e26f
Jahaziel Hernandez Hoyos <48532611+JahazielHernandezHoyos@users.noreply.github.com>
2022-02-17 17:00:55 -0500

    trato nuevamente a iterar las palabras por letra

--- trie.py
+++ trie.py
@@ -1,8 +1,16 @@
-def build_trie(*palabras):
+def build_trie(*words):
     arbol = {}
     nodo = arbol
-    for palabra in palabras:
-        print(palabra)
+    for palabra in words:
         if palabra == "":
             palabra = {}
+            return arbol
+        else:
+            palabra = palabra[0]
+        for letra in palabra:
+            if letra not in nodo:
+                nodo[letra] = {}
+            nodo = nodo[letra]
+        nodo[""] = None
+        nodo = arbol
     return arbol
