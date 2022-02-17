483d67276764f84e922037f9847960e05c5de2c0
Jahaziel Hernandez Hoyos <48532611+JahazielHernandezHoyos@users.noreply.github.com>
2022-02-17 17:49:31 -0500

    intento guardar las letras anteriores y sumarlas

--- trie.py
+++ trie.py
@@ -7,10 +7,13 @@ def build_trie(*words):
             return arbol
         for letra in palabra:
             #sumar la palabra anterior al nodo
+            print(letra)
             if letra in nodo:
+                letra = letra+1
                 nodo[letra].update(palabra)
                 nodo[letra].update(palabra[letra])
             else:
-                nodo[letra] = palabra[letra]
-            nodo = nodo.int([letra])
+                print(len(letra)-1)
+                nodo[letra] = palabra[len(letra)]
+            nodo = nodo[letra]
     return arbol
