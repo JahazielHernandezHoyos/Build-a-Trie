dda0ee1fd6c6072e099fd996761c07e5bbb82f80
Jahaziel Hernandez Hoyos <48532611+JahazielHernandezHoyos@users.noreply.github.com>
2022-02-17 18:06:44 -0500

    Vuelvo a empezar donde todo esta bien

--- trie.py
+++ trie.py
@@ -3,17 +3,10 @@ def build_trie(*words):
     for palabra in words:
         nodo = arbol
         if palabra == "":
-            palabra = {}
+            palabra = {palabra: None}
             return arbol
         for letra in palabra:
-            #sumar la palabra anterior al nodo
-            print(letra)
-            if letra in nodo:
-                letra = letra+1
-                nodo[letra].update(palabra)
-                nodo[letra].update(palabra[letra])
-            else:
-                print(len(letra)-1)
-                nodo[letra] = palabra[len(letra)]
-            nodo = nodo[letra]
+            nodo = nodo.setdefault(letra, {})
+        nodo[palabra] = None
+        #nodo = nodo.setdefault(None, None)
     return arbol
