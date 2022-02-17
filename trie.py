86d8e58f886d59c0c358e0a3e102afb90aa2c093
Jahaziel Hernandez Hoyos <48532611+JahazielHernandezHoyos@users.noreply.github.com>
2022-02-17 17:40:08 -0500

    Empiezo a tratar de ordenar de adentro a afuera

--- trie.py
+++ trie.py
@@ -3,10 +3,10 @@ def build_trie(*words):
     for palabra in words:
         nodo = arbol
         if palabra == "":
-            palabra = {}
+            palabra = {palabra: None}
             return arbol
         for letra in palabra:
             nodo = nodo.setdefault(letra, {})
-        nodo[None] = None
-        nodo = nodo.setdefault(None, None)
+        nodo[palabra] = None
+        #nodo = nodo.setdefault(None, None)
     return arbol
