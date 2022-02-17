60da78493406be80c89cc7b051116cd077a558c0
Jahaziel Hernandez Hoyos <48532611+JahazielHernandezHoyos@users.noreply.github.com>
2022-02-17 16:41:47 -0500

    Llego al mismo punto que llegue con javascript

--- trie.py
+++ trie.py
@@ -2,10 +2,7 @@ def build_trie(*palabras):
     arbol = {}
     nodo = arbol
     for palabra in palabras:
-        for letra in palabra:
-            if letra not in nodo:
-                nodo[letra] = {}
-            nodo = nodo[letra]
-        nodo['*'] = '*'
-        nodo = arbol
+        print(palabra)
+        if palabra == "":
+            palabra = {}
     return arbol
