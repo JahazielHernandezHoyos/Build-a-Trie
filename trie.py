eee26df089dabe1a11fbd39fdc9f19a4b5985986
Jahaziel Hernandez Hoyos <48532611+JahazielHernandezHoyos@users.noreply.github.com>
2022-02-17 16:35:58 -0500

    Trato de replicar lo que hice con javscript

--- trie.py
+++ trie.py
@@ -1,2 +1,11 @@
-def build_trie(*words):
-    return "Implement me! :)"
+def build_trie(*palabras):
+    arbol = {}
+    nodo = arbol
+    for palabra in palabras:
+        for letra in palabra:
+            if letra not in nodo:
+                nodo[letra] = {}
+            nodo = nodo[letra]
+        nodo['*'] = '*'
+        nodo = arbol
+    return arbol
